0a56402e4982c0c87b7bcf806e68069fdeaf1b63
webdevratul <mahibulislamratul@gmail.com>
2023-11-29 05:11:55 +0600

    Add CRUD operations for Blood Donation and Blog Posts

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
         const districtCollection = client.db("bloodDonationCampaign").collection("district");
         const upazilaCollection = client.db("bloodDonationCampaign").collection("upazila");
         const donationRequestCollection = client.db("bloodDonationCampaign").collection("donationRequest");
+        const blogCollection = client.db("bloodDonationCampaign").collection("blogPost");
 
         app.post("/user", async (req, res) => {
             const user = req.body;
@@ -90,6 +91,54 @@ async function run() {
             res.send(result);
         });
 
+        app.patch("/donationRequest/InProgress/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "InProgress"
+                }
+            }
+            const result = await donationRequestCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.patch("/donationRequest/Canceled/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "Canceled"
+                }
+            }
+            const result = await donationRequestCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.patch("/donationRequest/Pending/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "Pending"
+                }
+            }
+            const result = await donationRequestCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.patch("/donationRequest/Done/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "Done"
+                }
+            }
+            const result = await donationRequestCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
         app.get("/users/admin/:email", async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
@@ -141,6 +190,88 @@ async function run() {
 
         app.get("/donationRequest", async (req, res) => {
             const result = await donationRequestCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get("/donationRequest/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await donationRequestCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.put("/updateDonationRequest/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDonationRequest = req.body;
+
+
+            const donation = {
+                $set: {
+                    requesterName: updatedDonationRequest.requesterName,
+                    requesterEmail: updatedDonationRequest.requesterEmail,
+                    recipientDonnerEmail: updatedDonationRequest.recipientDonnerEmail,
+                    recipientName: updatedDonationRequest.recipientName,
+                    district: updatedDonationRequest.district,
+                    upazila: updatedDonationRequest.upazila,
+                    hospitalName: updatedDonationRequest.hospitalName,
+                    address: updatedDonationRequest.address,
+                    requestMessage: updatedDonationRequest.requestMessage,
+                    date: updatedDonationRequest.date,
+                    time: updatedDonationRequest.time,
+                }
+            }
+            const result = await donationRequestCollection.updateOne(filter, donation, options);
+            res.send(result);
+        });
+
+        app.delete("/DonationRequest/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await donationRequestCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.post("/blogPost", async (req, res) => {
+            const user = req.body;
+            const result = await blogCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.get("/blogPost", async (req, res) => {
+            const result = await blogCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.patch("/blogPost/publish/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "published"
+                }
+            }
+            const result = await blogCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.patch("/blogPost/unPublish/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "draft"
+                }
+            }
+            const result = await blogCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.delete("/blogPost/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await blogCollection.deleteOne(query);
             res.send(result);
         });
 
